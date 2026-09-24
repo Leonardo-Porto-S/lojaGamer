931df737cb4559a24eab48ecaab9e774179efa90
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-24 09:47:31 -0300

    feat: Codificando e estilizando o Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import {BrowserRouter as Router,Router,Route} from 'react-router-dom'
-import {Header} from './components/Header'
-import {Footer} from './components/Footer'
-import {GameCard} from './components/GameCard'
-import {Home} from './pages/Home'
-import {Contato} from './pages/Contato'
-import {Error} from './pages/Error'
-import {Jogos} from './pages/Jogos'
-import {Login} from './pages/Login'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import GameCard from './components/GameCard'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Error from './pages/Error'
+import Jogos from './pages/Jogos'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <Router>
       <div className="min-h-screen flex flex-col justify-between bg-[#141414] pt-4">
         <Header>
-          <Router>
-            <Rounte path='/' element={<Home/>}/>
-            <Rounte path='/jogos' element={<Jogos/>}/>
-            <Rounte path='/contato' element={<Contato/>}/>
-            <Rounte path='/login' element={<Login/>}/>
-            <Rounte path='*' element={<Error/>}/>
-          </Router>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/jogos' element={<Jogos/>}/>
+            <Route path='/contato' element={<Contato/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='*' element={<Error/>}/>
+          </Routes>
         </Header>
       </div>
     </Router>
